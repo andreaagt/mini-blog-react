d9de62cb6c0d38bd5f5e13bec03a61a881dcd3c6
andreagt3 <andgt3@gmail.com>
2021-11-16 08:04:43 -0600

    added create

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return ( 
+    <nav className="navbar">
+      <h1>Andrea's Blog</h1>
+      <div className="links">
+        {/* <a href="/">Home</a>
+        <a href="/create" style={{
+          color: "white",
+          backgroundColor: '#f1356d',
+          borderRadius: '8px',
+
+        }}>New Blog</a> */}
+        <Link to="/">Home</Link>
+        <Link to="/create">New Blog</Link>
+      </div>
+    </nav>
+  );
+}
+ 
+export default Navbar;
